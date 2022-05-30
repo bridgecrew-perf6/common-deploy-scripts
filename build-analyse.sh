b23e2b478cde2cc1043a9289177ff1c1c9e4ec19
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-30 19:52:22 +0530

    Added

---
+++ build-analyse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain test --domain-owner ${{ secrets.AWS_ACCOUNT_ID }} --query authorizationToken --output text`
+bash ./gradlew -Pbuild_target=SNAPSHOT clean build jacocoTestReport spotlessApply spotbugsMain
+bash ./gradlew -Pbuild_target=SNAPSHOT sonarqube showSonarQubeLink --info
