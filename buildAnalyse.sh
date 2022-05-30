b9f1aee3d0ec5f1e4dc7137dcf274b26dbac3e1b
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-30 22:23:23 +0530

    Getting Secrets from Main Repo

--- buildAnalyse.sh
+++ buildAnalyse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd initial
-export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain test --domain-owner ${{ secrets.AWS_ACCOUNT_ID }} --query authorizationToken --output text`
+export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain test --domain-owner $AWS_ACCESS_KEY --query authorizationToken --output text`
 bash ./gradlew -Pbuild_target=SNAPSHOT clean build jacocoTestReport spotlessApply spotbugsMain
 bash ./gradlew -Pbuild_target=SNAPSHOT sonarqube showSonarQubeLink --info
