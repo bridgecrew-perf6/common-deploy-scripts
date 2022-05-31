4d9ea8828870ee63781b0c5cc74561aea95cc439
GouthamSekar <goutham.sekar@nextuple.com>
2022-05-31 18:39:51 +0530

    Added Service name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ aws eks update-kubeconfig --name poc-cluster --region us-east-1
 export COLOR1="blue"
 export COLOR2="green"
 
-export SERVICE_ACTIVE_COLOR=$(kubectl get svc rest-uservice -n $ENVIRONMENT -o jsonpath='{.spec.selector.color}' --ignore-not-found=true)
+export SERVICE_ACTIVE_COLOR=$(kubectl get svc $SERVICE_NAME -n $ENVIRONMENT -o jsonpath='{.spec.selector.color}' --ignore-not-found=true)
 
 if [ "$SERVICE_ACTIVE_COLOR" == "$COLOR1" ]; then
   export SERVICE_PASSIVE_COLOR="$COLOR2"
